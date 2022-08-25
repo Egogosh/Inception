8ca9ecadbb682d38e6aef376416a48c241164123
Egogosh <81809151+Egogosh@users.noreply.github.com>
2022-08-25 20:23:42 +0300

    Update createdb.sql

--- srcs/requirements/mariadb/tools/createdb.sql
+++ srcs/requirements/mariadb/tools/createdb.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE wordpress;
+CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER 'scoach'@'%' IDENTIFIED BY '11122111';
 -- Для GRANT ALL привилегий для scoach, предоставляющих этому пользователю полный контроль над database wordpress
 -- Эти привилегии предназначены для wordpress применяются ко всем таблицам этой базы данных, 
